e21564e1fd6b9ddf663305faca71b8fd3f20ab38
Ihor Osadchyi <igorosa.com@gmail.com>
2022-07-03 19:02:45 +0300

    fix: products admin api links

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://ycq2813um2.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://ycq2813um2.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://ycq2813um2.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://fy8dpkazge.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://ycq2813um2.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://ycq2813um2.execute-api.eu-west-1.amazonaws.com/dev',
 };
