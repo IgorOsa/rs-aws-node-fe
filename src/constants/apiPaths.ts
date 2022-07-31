916168a9174b1949f48af8d63668651efc011a99
Ihor Osadchyi <igorosa.com@gmail.com>
2022-07-31 22:46:12 +0300

    feat: Add proxy https link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://ycq2813um2.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://j6ypilyg4i.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://ycq2813um2.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://igorosa-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'https://i7xzfpvs2i.execute-api.eu-west-1.amazonaws.com/api', // Proxied https to http://igorosa-cart-api-dev.eu-west-1.elasticbeanstalk.com/api
 };
 
 export default API_PATHS;
