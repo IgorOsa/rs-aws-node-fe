d617d467f65a61b14d48b0a68ae8466315b0bd19
Ihor Osadchyi <igorosa.com@gmail.com>
2022-08-14 22:47:14 +0300

    feat: Change api links

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,9 @@
-
 const API_PATHS = {
-  product: 'https://ycq2813um2.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://ycq2813um2.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://j6ypilyg4i.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://ycq2813um2.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://igorosa-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
+  product: "https://ycq2813um2.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://ycq2813um2.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://j6ypilyg4i.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "http://igorosa-bff-api-dev.eu-west-1.elasticbeanstalk.com",
+  cart: "https://i7xzfpvs2i.execute-api.eu-west-1.amazonaws.com/api", // Proxied https to http://igorosa-cart-api-dev.eu-west-1.elasticbeanstalk.com/api
 };
 
 export default API_PATHS;
